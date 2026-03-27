8aabc71f89f8406251ee4534304e1a02102e6d1a
Brian Collins <brian.collins@happyplaygrounds.com>
2026-03-27 15:31:37 -0500

    Update schema after migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_03_13_203634) do
+ActiveRecord::Schema[7.1].define(version: 2026_03_27_170056) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
